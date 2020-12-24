00892ee9b46d9d4613457d485ad943af42d7430e
sahil-777 <turbothunderboltz@gmail.com>
2020-12-24 17:25:07 +0530

    Added form-post-req

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const bodyParser = require('body-parser');
 var app= express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+app.get('/',function(req,res){
+    res.render('form');
+})
+
+app.post('/',function(req,res){
+    console.log(req.body.name);
+    console.log(req.body.age);
+    
+    res.render('form');
+})
+
 app.listen(3000,(err) =>{
     if(err) throw err;
     else
