c723afe473370ad2e67404d464f437b6f8e8aed2
sahil-777 <turbothunderboltz@gmail.com>
2020-12-24 19:52:17 +0530

    config updated

--- config.js
+++ config.js
@@ -7,11 +7,5 @@ var connection = mysql.createConnection({
 });
 connection.connect(function(err){
   if(err){console.log('Database connection failed!'); throw err};
-
-  connection.query('CREATE TABLE IF NOT EXISTS `formInfo` (`id` INT(11) NOT NULL AUTO_INCREMENT,`name` VARCHAR(255) NOT NULL,`age` INT(11) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1',function (err,result){
-      if(err){console.log('Table creation failed!'); throw err};
-     }
-  );
-  
 });
 module.exports = connection;
