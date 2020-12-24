cbc7ec736675d5a84b1076f3a7e0ea8740c822a9
sahil-777 <turbothunderboltz@gmail.com>
2020-12-24 18:44:17 +0530

    Inserted into database

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
+var connection = require('./config.js');
+
 app.get('/',function(req,res){
     res.render('form');
 })
@@ -12,7 +14,11 @@ app.get('/',function(req,res){
 app.post('/',function(req,res){
     console.log(req.body.name);
     console.log(req.body.age);
-    
+    var data ={
+        "name":req.body.name,
+        "age":req.body.age
+    };
+    connection.query('INSERT INTO forminfo SET ?',data);
     res.render('form');
 })
 
