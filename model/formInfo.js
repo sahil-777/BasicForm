9ed1d8603dac8b57594393a4df69d10e0bfe1c7d
sahil-777 <turbothunderboltz@gmail.com>
2020-12-24 19:50:38 +0530

    model folder

---
+++ model/formInfo.js
@@ -0,0 +1,8 @@
+var connection = require('./../config.js');
+connection.query('CREATE TABLE IF NOT EXISTS `formInfo` (`id` INT(11) NOT NULL AUTO_INCREMENT,`name` VARCHAR(255) NOT NULL,`age` INT(11) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1',function (err,result){
+    if(err){console.log('Table creation failed!'); throw err};
+    console.log('FormInfo Table created!');
+   }
+);
+
+ 
