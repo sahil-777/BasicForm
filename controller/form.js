fac3f6d879e4a43890ad1753b315ee8c2eaca1e9
sahil-777 <turbothunderboltz@gmail.com>
2020-12-29 03:53:51 +0530

    routes added

--- controller/form.js
+++ controller/form.js
@@ -1,2 +1,30 @@
-var connection = require('./config.js');
-var 
+const express = require('express');
+const connection = require('./../config.js');
+const router = express.Router();
+
+var createTable = require('./../model/formInfo.js');
+
+router.get('/',function(req,res){
+    createTable();
+    res.render('form');
+})
+
+router.post('/',function(req,res){
+    var data ={
+        "name":req.body.name,
+        "age":req.body.age
+    };
+    connection.query('INSERT INTO forminfo SET ?',data);
+    res.redirect('/showinfo');
+})
+
+router.get('/showinfo',function(req,res){
+    connection.query('SELECT * FROM forminfo',function(err,data){
+        if(err) throw err;
+        res.render('showInfo',{data:data}); 
+    })
+})
+
+module.exports = router;
+
+
